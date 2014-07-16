41ecb483eec119b772c4925759bfd46564d5472a
Sanjeev Sekar & J.J. Idt <sankumsek@gmail.com>
2014-07-16 14:22:13 -0700

    Final version of ToDo: Created Completed List, added background and modified colors

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,29 @@ $(document).ready(function() {
 
     var timeNow = $.now().toString();
 
-    $("ul#category-list").append("<li class='clickItem'>" + categoryItem + "</li>");
+    $("ul#category-list").append("<li class='clickItem clickable'>" + categoryItem + "</li>");
     $("ul#category-list").children("li").last().data("time-stamp", timeNow);
 
-    $("div.new-list").append("<div class=" + timeNow + " style='display: none;'' ><h2 id='category-header'>"+ categoryItem +"</h2><ul class ='items'></ul></div>").children("div").last().data("time-stamp", timeNow);;
+    $("div.new-list").append("<div class=" + timeNow + " style='display: none;'' ><h4 id='category-header'>"+ categoryItem +" List:</h4><ul class ='items'></ul></div>").children("div").last().data("time-stamp", timeNow);;
 
       $(".clickItem").click(function(){
         itemTime = $(this).data("time-stamp");
         $("div.new-list").children("div").hide();
         $("."+itemTime).show();
       });
+    $("#category").val("");
   });
   $("form#new-task").submit(function(event){
     event.preventDefault();
     var todoItem = $("input#add-task").val();
-    $("."+itemTime).children("ul").append("<li>"+ todoItem +"</li>");
+    $("."+itemTime).children("ul").append("<li class='clickable click-delete'>"+ todoItem +"</li>");
+    $("#add-task").val("");
+
+      $(".click-delete").last().click(function(){
+        var deletedItem = $(this).text();
+        $("#complete-list").append("<li class = 'deletes'>"+ deletedItem +"</li>");
+        $(this).remove();
+      });
   });
+
 });
