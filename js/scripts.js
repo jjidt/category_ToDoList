69f1068e98bf3b3b42fe9929bce977d4e0b858c5
Sanjeev Sekar & J.J. Idt <sankumsek@gmail.com>
2014-07-16 12:06:25 -0700

    refactored categories and adding tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
+  var itemTime;
   $("form#create-list").submit(function(event) {
     event.preventDefault();
 
     var categoryItem = $("input#category").val();
 
+    var timeNow = $.now().toString();
+
     $("ul#category-list").append("<li class='clickItem'>" + categoryItem + "</li>");
+    $("ul#category-list").children("li").last().data("time-stamp", timeNow);
 
-    $("input#category").val("");
+    $("div.new-list").append("<div class=" + timeNow + " style='display: none;'' ><h2 id='category-header'>"+ categoryItem +"</h2><ul class ='items'></ul></div>").children("div").last().data("time-stamp", timeNow);;
 
-    $("ul#category-list").children("li").last().click(function() {
-      $(".col-md-5").show();
-      $("h2#category-header").show();
-      $("div.new-list").append("<h2 id='category-header'>"+ categoryItem +"</h2><ul id ='items'></ul>");
-    });
-
-    // $("button#clear").click(function(){
-    //   $("ul#list").empty();
-    //   $("ul#done").empty();
-    //   $("h2#done-header").hide();
-    // });
+      $(".clickItem").click(function(){
+        itemTime = $(this).data("time-stamp");
+        $("div.new-list").children("div").hide();
+        $("."+itemTime).show();
+      });
+  });
+  $("form#new-task").submit(function(event){
+    event.preventDefault();
+    var todoItem = $("input#add-task").val();
+    $("."+itemTime).children("ul").append("<li>"+ todoItem +"</li>");
   });
 });
